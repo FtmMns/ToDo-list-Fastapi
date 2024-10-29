9d6f372a0715e1fc1d78e386652bbc8a28ab383d
Kian Anbarestani <kian.anb777@gmail.com>
2024-10-29 22:40:19 +0330

    updated

--- routers/address.py
+++ routers/address.py
@@ -4,7 +4,7 @@ sys.path.append("..")
 from typing import Optional
 from fastapi import Depends, APIRouter
 import models
-from database import engine, SessionLocal
+from database import SessionLocal
 from sqlalchemy.orm import Session
 from pydantic import BaseModel
 from .auth import get_current_user, get_user_exception
