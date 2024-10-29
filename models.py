8c5e59ab6d8396631c8a304aa1467544c823c096
Kian Anbarestani <kian.anb777@gmail.com>
2024-10-29 12:45:23 +0330

    updated

--- models.py
+++ models.py
@@ -15,7 +15,6 @@ class Users(Base):
     is_active = Column(Boolean, default=True)
     phone_number = Column(String)
     address_id = Column(Integer, ForeignKey("address.id"), nullable=True)
-
     todos = relationship("Todos", back_populates="owner")
     address = relationship("Address", back_populates="user_address")
 
